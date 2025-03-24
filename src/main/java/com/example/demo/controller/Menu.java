f446c73befdfa321a9ab1f3a1fe79e4ed73d831c
hch <hch728@nate.com>
2025-03-24 19:21:34 +0900

    commit

--- src/main/java/com/example/demo/controller/Menu.java
+++ src/main/java/com/example/demo/controller/Menu.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BuyVileInfo {
-    @GetMapping("buyVileInfo")
-    public String buyVileInfo(){
-        return "buyVileInfo";
+public class Menu {
+    @GetMapping("menu")
+    public String menu(){
+        return "menu";
     }
 }
--- src/main/java/com/example/demo/controller/UserInfoFind.java
+++ src/main/java/com/example/demo/controller/UserInfoFind.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BuyVileInfo {
-    @GetMapping("buyVileInfo")
-    public String buyVileInfo(){
-        return "buyVileInfo";
+public class UserInfoFind {
+    @GetMapping("userInfoFind")
+    public String userInfoFind(){
+        return "userInfoFind";
     }
 }
